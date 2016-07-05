cc38bf979d3d07798e9f03adc6973a09961193ea
Zach Gawlik <zachgawlik@Zachs-MacBook-Pro.local>
2016-07-05 14:53:12 -0400

    Hello world with react, webpack

---
+++ public/notedash.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './styles/base.scss';
+
+import Main from './components/Main';
+
+ReactDOM.render(<Main />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const webpack = require('webpack')
+const merge = require('webpack-merge');
+
+const TARGET = process.env.npm_lifecycle_event;
+const PATHS = {
+  public: path.resolve(__dirname, 'public'),
+  build: path.resolve(__dirname, 'build')
+};
+
+process.env.BABEL_ENV = TARGET;
+
+const common = {
+  entry: path.join(PATHS.public, 'notedash.jsx'),
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass'],
+      },
+      {
+        test: /\.jsx?$/,
+        include: PATHS.public,
+        loader: 'babel'
+      }
+    ]
+  }
+};
+
+if (TARGET === 'dev' || !TARGET) {
+  module.exports = merge(common, {
+
+    devtool: 'eval-source-map',
+
+    devServer: {
+      contentBase: PATHS.build,
+      historyApiFallback: true,
+      progress: true,
+      stats: 'errors-only',
+
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+  });
+} else if (TARGET === 'build') {
+  module.exports = merge(common, {
+    devtool: 'source-map'
+  });
+}
